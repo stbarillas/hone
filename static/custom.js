67b3a70a394173f9e39712dce096531c86d97016
stbarillas <stbarillas@gmail.com>
2019-04-29 21:50:41 -0700

    added chart rendering

--- static/custom.js
+++ static/custom.js
@@ -7,6 +7,10 @@ fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
     // Calls appendData function with json data as parameter
     .then(function(data){
         appendData(data);
+        return data
+    })
+    .then(function(data){
+        renderChart(data);
     })
     // Displays error if API call is unsuccessful
     .catch( function(err){
@@ -14,7 +18,7 @@ fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
         console.log(err)
     })
 
-// Creates dynamic table with json data and appends to dataTable element
+// Create dynamic table with json data and appends to dataTable element
 function appendData(data) {
     let priceDict = data["bpi"]
     let table = document.getElementById("dataTable")
@@ -29,3 +33,43 @@ function appendData(data) {
         col2.innerHTML = priceDict[key];
     });
 }
+
+// // Chart Code
+// var Chart = require('node_modules/chart.js/dist/Chart.js');
+// var ctx = document.getElementById('dataChart').getContext('2d');
+// var dataChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
