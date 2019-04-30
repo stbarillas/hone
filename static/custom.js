d797c6b8c8f317b1a99de48bda9b14f605384749
stbarillas <stbarillas@gmail.com>
2019-04-29 18:02:15 -0700

    added javascript function to create dynamic histrocial data tables

--- static/custom.js
+++ static/custom.js
@@ -1,19 +1,31 @@
 // API call function
 fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
+    // Converts API response to json
+    .then(function(response){
+        return response.json();
+    })
+    // Calls appendData function with json data as parameter
+    .then(function(data){
+        appendData(data);
+    })
+    // Displays error if API call is unsuccessful
+    .catch( function(err){
+        console.log("API fetch was unsuccessful");
+        console.log(err)
+    })
 
-var table = "";
-var rows = 2;
-var columns = 3;
-for(var r = 0; r < rows; r++){
-    table += "<tr>";
-    for(var c = 0; c < columns; c++){
-        table = "<td>" + c + "</td>";
-    }
-    table += "</tr>";
+// Creates dynamic table with json data and appends to dataTable element
+function appendData(data) {
+    let priceDict = data["bpi"]
+    let table = document.getElementById("dataTable")
+    // Reverses and loops through dictionary key:value pairs
+    let test = Object.keys(priceDict).reverse().forEach(function (key) {
+        // rows created dynamically
+        let row = table.insertRow(-1);
+        // two columns hardcoded until dynamic behavior needed
+        let col1 = row.insertCell(0);
+        let col2 = row.insertCell(1);
+        col1.innerHTML = key;
+        col2.innerHTML = priceDict[key];
+    });
 }
